3898482e069da3d05bc9748d5019ed867dc47d6f
Dominik Edgar Szpilski <kontaktedgarsz@gmail.com>
2022-06-02 18:48:02 +0200

    created structs to hold all bmp metadata

--- BMPMetadataReader.cpp
+++ BMPMetadataReader.cpp
@@ -1,6 +1,35 @@
 ﻿#include "export.h"
 #include <iostream>
 
+
+struct FileHeader {
+    short bfType;
+    int bfSize;
+    short bfReserved1;
+    short bfReserved2;
+    int bfOffBits;
+} File;
+
+struct PictureHeader {
+    int biSize;
+    int biWidth;
+    int biHeight;
+    short biPlanes;
+    short biBitCount;
+    int biCompression;
+    int biSizeImage;
+    int biXPelsPerMeter;
+    int biYPelsPerMeter;
+    int biClrUsed;
+    int biClrImportant;
+} Picture;
+
+struct KoloryRGB {
+    char R;
+    char G;
+    char B;
+} Rgb;
+
 int main()
 {
     std::cout << "Hello World!\n";
